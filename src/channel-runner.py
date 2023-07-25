7b3fc311d74d82c610e77671d05df234fe295670
sshcherbakov-uck <ss@goodsidehealth.com>
2023-07-25 10:23:39 -0500

    updates

--- src/channel-runner.py
+++ src/channel-runner.py
@@ -1,10 +1,12 @@
+import psycopg2
+from psycopg2 import sql
 import boto3
-import csv
 import os
 import time
 from db.mahler_conn import mahler_conn
-from db.easebase_conn  import easebase_conn
+from db.easebase_conn import easebase_conn
 from datetime import datetime
+import re
 
 # Initialize AWS S3 client
 s3 = boto3.client('s3')
@@ -19,79 +21,131 @@ eb_cursor = eb_conn.cursor()
 m_cursor.execute("SHOW TABLES")
 tables = m_cursor.fetchall()
 
-#use the mount in the task for the connection
-dir_path = '/easebase/'
-
-#use unix time to identify the run id, append the channel name after
-run_id = f"{int(time.time())}_mahler"
-phase = 's_load' # update the phase for different ecr steps
+# Define the constants
+run_id = int(time.time())
+phase = 's_load'
 table_name_prefix = 'stg.s_mahler_'
 log_table = 'logging.eb_log'
+channel = 'mahler'
 
-# For each table
+def remove_non_letters(input_string):
+    return re.sub(r'[^a-zA-Z ]', '', input_string)
+
+def sanitize_pg_name(pg_name):
+    # Remove or replace special characters in the column name
+    sanitized_pg_name = re.sub(r'[^a-zA-Z0-9_]+', '_', pg_name)
+    if sanitized_pg_name[0].isdigit():
+        sanitized_pg_name = "_" + sanitized_pg_name
+    return sanitized_pg_name
+
+
 for table in tables:
-    table = table[0]  # because fetchall() returns a list of tuples
-       # Get all data from the table
-    eb_cursor.execute("""
-        INSERT INTO {log_table}
-        (run_id, phase, run_source, run_target, run_status, latest_msg, start_ts)
-        VALUES ({run_id}, {phase}, {table}, {table_name_prefix}{table}, 'running', true, CURRENT_TIMESTAMP);
-    """) #logger 
-    m_cursor.execute(f"SELECT * FROM `{table}`;")
-    rows = m_cursor.fetchall()
-    
+    table = table[0]
+    pg_table = sanitize_pg_name(table)
+    target_table = f'{table_name_prefix}{pg_table}'
+
     try:
-     
+        # Log the start of processing
+        rsql=f"""
+            INSERT INTO {log_table}
+            (run_id, channel, phase, run_source, run_target, run_status, start_ts)
+            VALUES ({run_id}, '{channel}', '{phase}', '{table}', '{target_table}', 'running', CURRENT_TIMESTAMP);
+        """
+        eb_cursor.execute(rsql)
+        eb_conn.commit()
 
-        # Write the data to a csv file in the specified directory
-        file_path = os.path.join(dir_path, f"{table}_{datetime.now().strftime('%Y_%m_%d')}.csv")
-        with open(file_path, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            for row in rows:
-                writer.writerow(row)
+        # Fetch all rows from the mahler database
+        m_cursor.execute(f"SELECT * FROM `{table}`")
+        rows = m_cursor.fetchall()
 
+               # Fetch column names and types from the mahler database
+        m_cursor.execute(f"SHOW COLUMNS FROM `{table}`")
+        columns_data = m_cursor.fetchall()
+        columns_names = ', '.join([sanitize_pg_name(column[0]) for column in columns_data])
 
-        # Upload the csv file to S3 -- will be changed
-        # s3_key = f"easebase/s_loads/s_mahler/{os.path.basename(file_path)}"
-        # with open(file_path, 'rb') as data:
-        #     s3.upload_fileobj(data, 'uc4k-db', s3_key)
+        # Transform MySQL types to PostgreSQL types
+        def map_data_types(data_type):
+             # Define a dictionary to map MySQL to PostgreSQL data types
+            map_dict = {
+                "int": "bigint", # or integer if you know the numbers aren't very big
+                "tinyint": "smallint", 
+                "smallint": "smallint", 
+                "mediumint": "integer",
+                "bigint": "bigint",
+                "float": "double precision",
+                "double": "double precision",
+                "decimal": "decimal",
+                "date": "date",
+                "datetime": "timestamp without time zone",
+                "timestamp": "timestamp without time zone",
+                "time": "time without time zone",
+                "year": "integer",
+                "char": "character",
+                "varchar": "text",
+                "binary": "bytea",
+                "varbinary": "bytea",
+                "tinyblob": "bytea",
+                "tinytext": "text",
+                "blob": "bytea",
+                "text": "text",
+                "mediumblob": "bytea",
+                "mediumtext": "text",
+                "longblob": "bytea",
+                "longtext": "text",
+                "enum": "text",
+                "set": "text",
+            }
 
-        # If you want to keep the files locally, comment out the next line
-        os.remove(file_path)
+            # Use the map_dict dictionary to map the data types
+            return map_dict.get(data_type, "text") # Default to "text" if data type is not found
+        
+        columns_with_types = ', '.join([f"{sanitize_pg_name(column[0])} {map_data_types(column[1])}" for column in columns_data])
+
+        # Check if the target table exists in the easebase database
+        eb_cursor.execute(f"SELECT count(*) FROM information_schema.tables WHERE table_name = '{target_table}'")
+        table_exists = eb_cursor.fetchone()[0]
+
+        # Create backup table in the easebase database
+        if table_exists:
+            # If the table exists, create a backup table
+            backup_table = f'{target_table}_bck'
+            eb_cursor.execute(f"DROP TABLE IF EXISTS {backup_table}")
+            eb_cursor.execute(f"CREATE TABLE {backup_table} AS SELECT * FROM {target_table}")
+
+        # Create new table in the easebase database
+        #print(f"CREATE TABLE {target_table} ({columns_with_types})")
+        eb_cursor.execute(f"DROP TABLE IF EXISTS {target_table}")
+        eb_cursor.execute(f"CREATE TABLE {target_table} ({columns_with_types})")
+        
+        # Insert each row to the easebase database
+        for row in rows:
+            insert_sql = sql.SQL(f"INSERT INTO {target_table} ({columns_names}) VALUES %s")
+            eb_cursor.execute(insert_sql, (row,))
+        eb_conn.commit()
+
+        # Update the log record for this run_id and table to success
+        rsql=f"""
+            UPDATE {log_table}
+            SET run_status = 'success', end_ts = CURRENT_TIMESTAMP
+            WHERE run_id = {run_id} AND run_source = '{table}' and channel = '{channel}';
+        """
+        eb_cursor.execute(rsql)
+        eb_conn.commit()
 
     except Exception as e:
-        if table != 'pusers':
-                sql="""
+        #if table != 'pusers':
+                err = remove_non_letters(str(e))
+                rsql=f"""
                     UPDATE {log_table}
-                    SET latest_msg = false
-                    WHERE run_id = {run_id} AND run_source = {table} AND latest_msg = true;
-                    """
-                eb_cursor.execute(sql)
-                eb_conn.commit()  # Commit the log record update to the database
-
-                sql="""
-                    UPDATE {log_table}
-                    SET run_status = 'failure', error_desc = %s, latest_msg = true, end_ts = CURRENT_TIMESTAMP
-                    WHERE run_id = {run_id} AND run_source = {table};
-                """, (str(e))
-                eb_cursor.execute()
+                    SET run_status = 'failure', error_desc = '{err[:255]}', end_ts = CURRENT_TIMESTAMP
+                    WHERE run_id = {run_id} AND run_source = '{table}' and channel = '{channel}';
+                """
+                eb_cursor.execute(rsql)
                 eb_conn.commit()
-        continue
-    finally:
-                 # Update all previous log records for this run_id and table to not be the latest
-        eb_cursor.execute("""
-            UPDATE {log_table}
-            SET latest_msg = false
-            WHERE run_id = {run_id} AND run_source = {table} AND latest_msg = true;
-        """)
-        eb_conn.commit()  # Commit the log record update to the database
-
-        #then i am going to log table success
-        eb_cursor.execute("""
-            UPDATE {log_table}
-            SET run_status = 'success', latest_msg = true, end_ts = CURRENT_TIMESTAMP
-            WHERE run_id = {run_id} AND run_source = {table};
-        """)
+        #continue
+                   # Update all previous log records for this run_id and table to not be the latest
+        
 
 # Remember to close the connection when you're done
 m_conn.close()
+eb_conn.close()
