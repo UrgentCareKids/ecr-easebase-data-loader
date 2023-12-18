74e35df54c481ba278ecbeb08aa18050eda0f049
sshcherbakov-uck <ss@goodsidehealth.com>
2023-12-18 10:41:50 -0600

    incorrect password flag

--- src/mahler_stg_pull_filedump.py
+++ src/mahler_stg_pull_filedump.py
@@ -185,7 +185,7 @@ for table in tables:
         mysql_database=get_mahler_param('database')
         
         dump_file_path = os.path.join(dir_path, f"{table}_{datetime.now().strftime('%Y_%m_%d')}.sql")
-        dump_command = f"mysqldump -u {mysql_username} -p {mysql_password} {mysql_database} {table} > {dump_file_path}"
+        dump_command = f"mysqldump -u {mysql_username} --password {mysql_password} {mysql_database} {table} > {dump_file_path}"
         subprocess.run(dump_command, shell=True, check=True)
 
         # Open the exported file and load it into PostgreSQL
