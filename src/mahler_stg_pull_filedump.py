8056ceb31e6fddb0c3a60ca2ad8de2288c188ad3
sshcherbakov-uck <ss@goodsidehealth.com>
2023-12-19 14:21:57 -0600

    updates to delimiter

--- src/mahler_stg_pull_filedump.py
+++ src/mahler_stg_pull_filedump.py
@@ -183,12 +183,16 @@ for table in tables:
         # subprocess.run(cmd, shell=True, check=True)
     
         # Step 1: Dump data to a temporary file
-        mysql_cmd = f"mysql --default-character-set=utf8 -u {mysql_username} -p{mysql_password} -h 127.0.0.1 -P 3306 -e \"USE {mysql_database}; SELECT * FROM {table}\" > {tmp_file_path}"
+
+        # Step 1: Dump data to a temporary file
+        temp_file_path = file_path
+        mysql_cmd = f"mysql --default-character-set=utf8 -u {mysql_username} -p{mysql_password} -h 127.0.0.1 -P 3306 -e \"USE {mysql_database}; SELECT * FROM {table}\" > {temp_file_path}"
         subprocess.run(mysql_cmd, shell=True, check=True)
 
         # Step 2: Process the temporary file with awk and write to the final file
-        process_cmd = f"awk '{{gsub(\"\\t\", \"\\x1F\"); print}}' {tmp_file_path} > {file_path}"
+        process_cmd = f"awk '{{gsub(\"\\t\", \"\\x1F\"); print}}' {temp_file_path} > {file_path}"
         subprocess.run(process_cmd, shell=True, check=True)
+
       
         with open(file_path, 'r') as f:
             eb_cursor.copy_expert(f"COPY {schema}.{target_table} FROM STDIN WITH (FORMAT 'csv', DELIMITER E'\x1F')", f)
